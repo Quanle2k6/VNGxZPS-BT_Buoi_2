f536e71bf3d30d279a8925a8a93e6f93e1abd761
Lê Hoàng Quân <quanle2006295@gmail.com>
2026-09-22 21:02:22 +0700

    feat: add some spotlight's controls

--- godot-practice/test/diorama.gd
+++ godot-practice/test/diorama.gd
@@ -8,8 +8,22 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	var turn_on_off = Input.is_action_just_pressed("turn_on_off_spotlight")
+	var increase_energy_light = Input.is_action_just_pressed("increase_energy_light")
+	var decrease_energy_light = Input.is_action_just_pressed("decrease_energy_light")
+	var twist_spotlight_Left = Input.is_action_just_pressed("twist_spotlight_left")
+	var twist_spotlight_Right = Input.is_action_just_pressed("twist_spotlight_right")
+
+
 	if turn_on_off:
 		if $SpotLight3D.is_visible_in_tree():
 			$SpotLight3D.visible = false
 		else:
 			$SpotLight3D.visible = true
+	if increase_energy_light:
+		$SpotLight3D.light_energy += 0.5
+	if decrease_energy_light:
+		$SpotLight3D.light_energy -= 0.5
+	if twist_spotlight_Left:
+		$SpotLight3D.rotate_z(-0.1)
+	if twist_spotlight_Right:
+		$SpotLight3D.rotate_z(0.1)
